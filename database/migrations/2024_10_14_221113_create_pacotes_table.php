db7d4c4a52f81aed1c71b33fce844cf1ac0c1a6b
ThomasLeyer2231 <138226598+ThomasLeyer2231@users.noreply.github.com>
2024-10-14 19:44:35 -0300

    Arquivo principal
    Não tem o vendor

---
+++ database/migrations/2024_10_14_221113_create_pacotes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacotes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome');
+            $table->float('peso');
+            $table->integer('quantidade');
+            $table->boolean('fragilidade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacotes');
+    }
+};
